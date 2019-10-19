29e5c15d044eb91d75155dc2cc4ac129d9fceec1
Tavis Weeden <tavisweeden@gmail.com>
2019-10-19 15:42:18 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,9 @@
-def hello_t
-#   i = 0
+def hello_t(array)
+   i = 0
 
-#   while i < array.length
-#     yield array[i]
-#       i = i + 1
+   while i < array.length
+     yield array[i]
+       i = i + 1
   end
 end
 
